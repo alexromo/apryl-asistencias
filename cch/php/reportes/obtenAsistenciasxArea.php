157745ef839627044348811f2ae2e8e660d2b495
Alejandro Romo Zavala <alexromozavala@gmail.com>
2009-08-16 02:08:18 -0500

    Se modifico el Resumen de Asistencias para que agregara Faltas Justificadas Se modifico el query del detalle de faltas para que mostrara faltas justificadas

--- cch/php/reportes/obtenAsistenciasxArea.php
+++ cch/php/reportes/obtenAsistenciasxArea.php
@@ -37,17 +37,21 @@ else
                 $table =  '<table width="95%" cellspacing="1" >'
                          .'<thead><tr><th colspan="2">RESUMEN</th></tr></thead>'
                          .'<tbody>';
+                $asistencias=$faltas=$faltasjustificadas=0;         
                 foreach ($rs as $k => $row)
                     {    
                         if ($row[0] == 1)
                             $asistencias = $row[1];
-                        else 
+                        if ($row[0] == 2)
                             $faltas = $row[1];
+                        if ($row[0] == 3)
+                            $faltasjustificadas = $row[1];
                     }
-                    $table .= '<tr><th width="150">ASISTENCIAS</th><td><b>'.$asistencias.'</b> ('.sprintf("%01.2f",($asistencias/($faltas+$asistencias)*100)).'%)</td></tr>'; 
-                    $table .= '<tr><th width="150">FALTAS</th><td><b>'.$faltas.'</b> ('.sprintf("%01.2f",($faltas/($faltas+$asistencias)*100)).'%)</td></tr>';   
+                    $table .= '<tr><th width="150">ASISTENCIAS</th><td><b>'.$asistencias.'</b> ('.sprintf("%01.2f",($asistencias/($faltas+$faltasjustificadas+$asistencias)*100)).'%)</td></tr>'; 
+                    $table .= '<tr><th width="150">FALTAS</th><td><b>'.$faltas.'</b> ('.sprintf("%01.2f",($faltas/($faltas+$faltasjustificadas+$asistencias)*100)).'%)</td></tr>';
+                    $table .= '<tr><th width="150">FALTAS JUSTIFICADAS</th><td><b>'.$faltasjustificadas.'</b> ('.sprintf("%01.2f",($faltasjustificadas/($faltas+$faltasjustificadas+$asistencias)*100)).'%)</td></tr>';   
                 $table .= '</tbody></table><br />';
-                $js = '<input type="button" value="Muestra/Oculta Grafica " onclick="DrawPieChart('.$asistencias.','.$faltas.');$(\'graphcontainer\').toggle();" />';
+               $js = '<script>DrawPieChart('.$asistencias.','.$faltas.','.$faltasjustificadas.');</script><input type="button" value="Muestra/Oculta Grafica " onclick="DrawPieChart('.$asistencias.','.$faltas.','.$faltasjustificadas.');$(\'graphcontainer\').toggle();" />';
                 
                 # Asistencias Detalle
                 
@@ -79,12 +83,12 @@ else
 
                 # Faltas Detalle
 
-                $query = "SELECT  ma.idMateria,ma.Materia,count(*)"
+                $query = "SELECT  ma.idMateria,ma.Materia,SUM(IF (ap.idStatus = 2,1,0)),SUM(IF (ap.idStatus = 3,1,0)),COUNT(*)"
                         ." FROM AsistenciaProfesores ap, Rondin rd, ProfesorSalon ps, Materia ma, Area ar" 
                         ." WHERE ap.idRondin = rd.idRondin"
                         ." AND rd.status = 'S'"
                         ." AND rd.idCicloEscolar = ".$idCicloEscolar
-                        ." AND ap.idStatus = 2"
+                        ." AND ap.idStatus in  (2,3)"
                         ." AND ap.idProfesorSalon = ps.idProfesorSalon"
                         ." AND ps.idMateria = ma.idMateria"
                         ." AND ma.idArea = ar.idArea"
@@ -95,11 +99,11 @@ else
                 if (!$rs->fields) $table2 = '<div class="error">No hay Faltas Registradas</div>';
                 else {
                         $table2 = '<table width="95%" cellspacing="1" >'
-                                 .'<thead><tr><th colspan="5">FALTAS</th></tr><tr><th>id</th><th>Materia</th><th>Asistencias</th></thead>'
+                                 .'<thead><tr><th colspan="5">FALTAS</th></tr><tr><th>id</th><th>Materia</th><th>Faltas</th><th>F. Just.</th><th>Total</th></thead>'
                                  .'<tbody>';
                         foreach ($rs as $k => $row)
                             {
-                                $table2 .= "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td>";
+                                $table2 .= "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]."</td><td>".$row[4]."</td>";
                             }
                         $table2 .= '</tbody></table><br />';
                 }
